c21c42a0b15c110237897cd10342b5a0ac29ade3
gafeys <glenn.feys@ugent.be>
2019-10-30 22:57:11 +0100

    now you can remove images also fixed some things like animation

--- admin.php
+++ admin.php
@@ -6,13 +6,13 @@ $password = "admin";
 $currentFile = '';
 $content = '';
 $webpages = [];
-
 //if there is no password given yet, ask the user for the password
 if (!isset($_POST["password"]) || (isset($_POST["password"]) && $_POST["password"] !== $password)) { // TODO: should change to hash + session/token auth
     $error = '';
     if (isset($_POST["password"]) && $_POST["password"] !== $password) {
         $error = 'Wrong password !<br>';
     }
+    var_dump($_POST);
     die($error.'
     <form method="post" action="admin.php">
     password: <input type="password" name="password"> <br>
@@ -52,6 +52,11 @@ if (isset($_FILES["fileToUpload"])) {
     }
 
     die();
+}
+
+if (isset($_POST["fileDelete"]) && is_file($_POST["fileDelete"])) {
+    unlink($_POST["fileDelete"]);
+    die("succesfully deleted file");
 }
 
 //this is executed when 'Save' is pressed and will set the value to the editted value
@@ -149,6 +154,7 @@ foreach ($images as $image) {
 
 $additionalContent = '
     <body>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <style type="text/css" scoped>
     .modal {
         display: none; /* Hidden by default */
@@ -169,11 +175,13 @@ $additionalContent = '
         margin: 15% auto; /* 15% from the top and centered */
         padding: 20px;
         border: 1px solid #888;
-        width: 80%; /* Could be more or less, depending on screen size */
+        width: 50%; /* Could be more or less, depending on screen size */
+        height: 600px;
       }
       
       /* The Close Button */
       .close {
+        top: 10px;
         color: #aaa;
         float: right;
         font-size: 28px;
@@ -195,29 +203,34 @@ $additionalContent = '
             <h2>change image to...</h2>
             <select id="imgselect" onchange="setPreview()">'.$options.'</select>
             <br>
-            <img id="previewImg" style="width: inherit; margin: 20px auto;" src=""/>
-            <input type="submit" value="change" onclick="setImg()">
-
+            <img id="previewImg" style="width: auto; height: 320px; margin: 20px auto;" src=""/>
+            <br>
+            <input id="setI" type="submit" value="change" onclick="setImg()">
+            <input id="removeI" type="submit" value="remove" onclick="removeImg()">
             </div>
         
         </div>
-        <div id="editBar" style="position:fixed; bottom:0; right:0; z-index: 9; padding: 10px;background-color: #f7faff; box-shadow: inset -4px -5px 34px -6px rgba(0,0,0,0.67);">
-            <div id="toggle" style="width:100%; height:25px; background-color: #c4c7cc;text-align: center;" onclick="toggleV()">Toggle visibility</div>
-            <h3>Editor</h3>
-            <button  onclick="document.execCommand(\'bold\',false,null);">Bold</button>
-            <button  onclick="document.execCommand(\'italic\',false,null);">Italic</button>
-            <button  onclick="document.execCommand(\'underline\',false,null);">underline</button>
-            <br>
-            <button  onclick="document.execCommand(\'decreaseFontSize\',false,null);">smaller</button>
-            <button  onclick="document.execCommand(\'increaseFontSize\',false,null);">bigger</button>
-            <br>
-            <form id="uploadForm" action="admin.php" method="post" enctype="multipart/form-data">
-                <input type="file" name="fileToUpload" id="fileToUpload" onchange="submitUpload()">
-            </form>
-            <input style="width:100%;" type="button" id="saveBtn" value="Save" onclick="saveFile()"/>
-            <br>
-            '.$links.'
-            <br>
+        <div id="editBar" style="position:fixed; bottom:0; right:0; z-index: 9; background-color: #f7faff; box-shadow: inset -4px -5px 34px -6px rgba(0,0,0,0.67); max-height: 1000px; transition: max-height 1s;">
+            <div id="toggle" style="width:100%; padding:10px; background-color: #c4c7cc;text-align: center;" onclick="toggleV()"><i id="toggleBtn" class="fas fa-chevron-down"></i></div>
+            <div style="margin:10px;">
+                <h3>Editor</h3>
+                <button  onclick="document.execCommand(\'bold\',false,null);"><i class="fas fa-bold"></i></button>
+                <button  onclick="document.execCommand(\'italic\',false,null);"><i class="fas fa-italic"></i></button>
+                <button  onclick="document.execCommand(\'underline\',false,null);"><i class="fas fa-underline"></i></button>
+                <br>
+                <button  onclick="document.execCommand(\'decreaseFontSize\',false,null);">smaller</button>
+                <button  onclick="document.execCommand(\'increaseFontSize\',false,null);">bigger</button>
+                <br>
+                <form id="uploadForm" action="admin.php" method="post" enctype="multipart/form-data">
+                    <input type="file" name="fileToUpload" id="fileToUpload" onchange="submitUpload()">
+                </form>
+                <button  onclick="callRemove()">Remove Image</button>
+                <input style="width:100%;" type="button" id="saveBtn" value="Save" onclick="saveFile()"/>
+                <br>
+                '.$links.'
+                <br>
+            </div>
+            
             
         </div>
 
@@ -248,9 +261,13 @@ $additionalContent = '
 
             function toggleV() {
                 if (toggled) {
-                    document.getElementById("editBar").style.height = "40px";
+                    document.getElementById("editBar").style.maxHeight = "35px";
+                    document.getElementById("toggleBtn").classList.add("fa-chevron-up");
+                    document.getElementById("toggleBtn").classList.remove("fa-chevron-down");
                 } else {
-                    document.getElementById("editBar").style.height = "auto";
+                    document.getElementById("editBar").style.maxHeight = "1000px";
+                    document.getElementById("toggleBtn").classList.add("fa-chevron-down");
+                    document.getElementById("toggleBtn").classList.remove("fa-chevron-up");
                 }
                 toggled = !toggled;
             }
@@ -268,7 +285,36 @@ $additionalContent = '
                     body: formData,
                 })
                 .then(response => response.text())
-                .then(response => alert(response))
+                .then(response => {
+                    alert(response);
+                    if (response === "Your file was succesfully uploaded") {
+                        let x = document.getElementById("imgselect");
+                        let option = document.createElement("option");
+                        option.text = ".\\\\CMSimpleImages\\\\"+image.name;
+                        option.value = ".\\\\CMSimpleImages\\\\"+image.name;
+                        x.add(option);
+                    }
+                });
+            }
+
+            function removeImg() {
+                let e = document.getElementById("imgselect");
+                let value = e.options[e.selectedIndex].value;
+                const formData = new FormData();
+                formData.append("password", "' . $password . '");
+                formData.append("fileDelete", value);
+
+                fetch("admin.php", {
+                    method: "POST",
+                    body: formData,
+                })
+                .then(response => response.text())
+                .then(response => {
+                    alert(response);
+                    if (response === "succesfully deleted file") {
+                        e.remove(e.selectedIndex);
+                    }
+                });
             }
 
             var imgEditting;
@@ -277,6 +323,8 @@ $additionalContent = '
                 $("img").click(function(){
                     imgEditting = this;
                     modal.style.display = "block";
+                    document.getElementById("setI").style.display = "block";
+                    document.getElementById("removeI").style.display = "none";
                  });
             });
 
@@ -284,12 +332,20 @@ $additionalContent = '
                 $("img").click(function(){
                     imgEditting = this;
                     modal.style.display = "block";
+                    document.getElementById("setI").style.display = "block";
+                    document.getElementById("removeI").style.display = "none";
                 });
                 modal = document.getElementById("myModal");
                 span = document.getElementsByClassName("close")[0];
                 span.onclick = function() {
                     modal.style.display = "none";
                 }
+            }
+
+            function callRemove() {
+                modal.style.display = "block";
+                document.getElementById("setI").style.display = "none";
+                document.getElementById("removeI").style.display = "block";
             }
             
             function setImg() {
